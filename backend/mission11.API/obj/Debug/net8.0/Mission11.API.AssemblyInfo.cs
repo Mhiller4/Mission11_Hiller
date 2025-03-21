af31d8493e2306f1d5925f2fcc276762c56903b0
Mekeli Hiller <mekelihiller@gmail.com>
2025-03-21 16:27:13 -0500

    backend commit

--- backend/mission11.API/obj/Debug/net8.0/Mission11.API.AssemblyInfo.cs
+++ backend/mission11.API/obj/Debug/net8.0/Mission11.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission11.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c62f837bc0ecfd59e6cce54f9bb649c8c8b4b1bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d086f46191585a1fe65fc83735d35576a807660b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission11.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission11.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
